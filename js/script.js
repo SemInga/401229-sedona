64e551a865b672aed0caa32a999c7f9242cb2300
SemInga <inga.semenova27@gmail.com>
2018-03-31 19:58:41 +0700

    Оживить интерактивные элементы в личном проекте

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 var searchButton = document.querySelector("button.search");
 var shortSearch = document.querySelector(".short-search");
-
-searchButton.addEventListener("click", function (evt) {
+var arrivalDate = document.querySelector (".arr-date");
+var departureDate = document.querySelector (".dep-date");
+var amountAdults = document.querySelector (".ad-am");
+var amountChildren = document.querySelector (".ch-am");
+  
+  searchButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   shortSearch.classList.toggle("short-search-open");
 });
+
+  shortSearch.addEventListener("submit", function (evt) {
+    if (!arrivalDate.value || !departureDate.value || !amountAdults.value || !amountChildren.value) {
+      evt.preventDefault();
+    }
+  });
+
