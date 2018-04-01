c43620d4a85f0a39e38a585cfe7eba7f0035b737
SemInga <inga.semenova27@gmail.com>
2018-04-01 18:36:50 +0700

    Финальная проверка

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 var searchButton = document.querySelector("button.search");
 var shortSearch = document.querySelector(".short-search");
-var arrivalDate = document.querySelector (".arr-date");
-var departureDate = document.querySelector (".dep-date");
-var amountAdults = document.querySelector (".ad-am");
-var amountChildren = document.querySelector (".ch-am");
+var arrivalDate = document.querySelector(".arr-date");
+var departureDate = document.querySelector(".dep-date");
+var amountAdults = document.querySelector(".ad-am");
+var amountChildren = document.querySelector(".ch-am");
   
-  searchButton.addEventListener("click", function (evt) {
+searchButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   shortSearch.classList.toggle("short-search-open");
 });
 
-  shortSearch.addEventListener("submit", function (evt) {
-    if (!arrivalDate.value || !departureDate.value || !amountAdults.value || !amountChildren.value) {
-      evt.preventDefault();
-    }
-  });
+shortSearch.addEventListener("submit", function (evt) {
+  if (!arrivalDate.value || !departureDate.value || !amountAdults.value || !amountChildren.value) {
+    evt.preventDefault();
+  }
+});
 
